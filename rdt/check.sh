76f7893dcdc6061b98aa629ae2ec411a871dfb67
Ethereal <m15635418665@163.com>
2023-02-27 11:24:49 +0800

    fix: use crc

---
+++ rdt/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+error=0
+max=500
+for ((i=0; i<$max; i++));
+do
+    yes "\n" | ./rdt_sim 1000 0.1 100 0.5 0.5 0.3 0 | grep "Congratulations" >> /dev/null
+    if [ $? -eq 1 ]; then
+        error=$(($error + 1))
+    fi
+done
+
+echo $error"/"$max
